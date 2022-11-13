7d0263cc67cdb5c28ecc964073d5b1a76b3ef582
Daniel <daniel-mcintyre@hotmail.com>
2022-11-13 11:55:00 -0500

    initial backend setup with server connection to DB

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const port = process.env.PORT;
+const db = require('./config/db')
+
+db();
 
 const app = express();
+app.use(cors());
 
 app.listen(port, console.log(`listening on port ${port}!`));
