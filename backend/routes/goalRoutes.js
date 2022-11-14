f027b60ec9449414b8dda4ca1cfc307cefe47767
Daniel <daniel-mcintyre@hotmail.com>
2022-11-14 14:14:54 -0500

    protected user getUser and Goal routes with JWT

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const {getGoals, addGoals, deleteGoals} = require('../controllers/goalControllers');
+const {getGoals, addGoals, deleteGoals
+} = require('../controllers/goalControllers');
+const { protect } = require('../middleware/authMiddleware');
 
-router.get('/', getGoals);
-router.post('/', addGoals);
-router.delete('/:id', deleteGoals);
+router.get('/', protect, getGoals);
+router.post('/', protect, addGoals);
+router.delete('/:id', protect, deleteGoals);
 
 module.exports = router;
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -2,9 +2,10 @@ const express = require('express');
 const router = express.Router();
 const { registerUser, loginUser, getUser 
 } = require('../controllers/userControllers');
+const {protect} = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 router.post('/login', loginUser);
-router.get('/users', getUser);
+router.get('/users', protect, getUser);
 
 module.exports = router;
