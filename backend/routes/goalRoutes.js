980f6b21e2be54ce64bf1a725f91df2e906792e3
Daniel <daniel-mcintyre@hotmail.com>
2022-11-14 09:46:39 -0500

    added post and delete functionality to routes

--- backend/routes/goalRoutes.js
+++ backend/routes/goalRoutes.js
@@ -5,6 +5,18 @@ router.get('/', (req, res) => {
     res.json({
         message: 'Get Goals'
     })
+});
+
+router.post('/', (req, res) => {
+    res.json({
+        message: 'Add Goals'
+    })
+});
+
+router.delete('/:id', (req, res) => {
+    res.json({
+        message: `Delete goal ${req.params.id}`
+    })
 })
 
 module.exports = router;
