07f64c69ca25f0759c20554ce04590e9f3f5e5ec
Daniel <daniel-mcintyre@hotmail.com>
2022-11-14 11:03:15 -0500

    implemented Create Read and Delete functionality to controller functions

--- backend/controllers/goalControllers.js
+++ backend/controllers/goalControllers.js
@@ -1,23 +1,42 @@
 const asyncHandler = require('express-async-handler');
 
+const Goal = require('../models/goalModels');
+
 const getGoals = asyncHandler(async(req, res) => {
-    res.json({
-        message: 'Get Goals'
-    })
+    const goals = await Goal.find({
+    })   
+    res.json({goals})
 });
 
 const addGoals = asyncHandler(async(req, res) => {
+
+    const goal = await Goal.create({
+        text: req.body.text
+    })
+
     if(!req.body.text) {
         res.status(400)
     throw new Error('Please add a text feild')
     }
 
     res.json({
-        message: 'Goals Added'
+        goal
     })
 });
 
 const deleteGoals = asyncHandler(async(req, res) => {
+
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal) {
+        res.status(400)
+        throw Error('Goal not found')
+    }
+
+    await Goal.remove(goal)
+
+    res.json({ id: req.params })
+
     res.json({
         message:`Delete goal ${req.params.id}`
     })
