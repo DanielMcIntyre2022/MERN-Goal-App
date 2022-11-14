98e8f456105dea42f3a649916919017b22d0a03b
Daniel <daniel-mcintyre@hotmail.com>
2022-11-14 13:30:04 -0500

    Created JWT function and implemented it in registerUser and loginUser

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -39,7 +39,8 @@ const registerUser = asyncHandler(async(req, res) => {
         res.status(201).json({
             _id: user.id, 
             name: user.name,
-            email: user.email
+            email: user.email,
+            token: generateToken(user.id)
         })
     } else {
         res.status(400)
@@ -57,7 +58,8 @@ const loginUser = asyncHandler(async(req, res) => {
         res.json({
             _id: user.id,
             name: user.name, 
-            email: user.email
+            email: user.email, 
+            token: generateToken(user._id)
         })
     } else {
         res.status(400)
@@ -70,6 +72,14 @@ const getUser = asyncHandler(async(req, res) => {
         message: 'user data found'
     })
 });
+
+// Generate JWT //
+
+const generateToken = (id) => {
+    return jwt.sign({id}, process.env.JWT_SECRET, {
+        expiresIn: '30d'
+    })
+};
 
 module.exports = {
     registerUser,
